af1b318e36c4fc0b8764bb288deb1407b08528e8
djkeu <mrkooij@gmail.com>
2023-09-03 17:00:45 +0200

    Renam index2 to index

---
+++ index.py
@@ -0,0 +1,27 @@
+# Project 1: Alien invasion, p.225
+
+
+# Chapter 12: A ship that fires bullets, p.227
+""" 
+Planning your project, p.228
+Installing pygame, p.228
+Startomg the game project, p.229
+    Creating a pygame window and responding to user input, p.229
+        alien_invasion.py
+    Setting the background color, p.230
+    Creating a settings class, p.231
+        settings.py
+    Adding the ship image, p.232
+    Creating the ship class, p.233
+    Drawing the ship to the screen, p.235
+Refactoring: _check_events() and _update_screen(), p.236
+    The _check_events() methogd, p.236
+    The _update_screen() method, p.237
+Try it yourself, p.238
+    raiders.py
+    12.1 Blue sky, p.238
+    12.2 Game character, p.238
+Piloting the ship, p.238
+    Responding to a keypress, p.238
+    
+"""
