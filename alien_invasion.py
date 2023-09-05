e62e1d55dacd60926e0d159ed4cb9963a80b2839
djkeu <mrkooij@gmail.com>
2023-09-05 22:40:29 +0200

    Add explanatory comments

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,21 +32,23 @@ class AlienInvasion:
     def _check_events(self):
         """Listen to mouse and keyboard input."""
         for event in pygame.event.get():
+            # Quit the game
             if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 sys.exit()
 
+            # Move the ship
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
                 elif  event.key == pygame.K_LEFT:
                     self.ship.moving_left = True
 
+            # Stop moving the ship
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
                 elif event.key == pygame.K_LEFT:
                     self.ship.moving_left = False
-    
 
     def _update_screen(self):
         """Update images on screen, flip to new screen."""
