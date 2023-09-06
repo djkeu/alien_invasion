3a586a1512a65ee865a19c2f03ff3727de0cc62d
djkeu <mrkooij@gmail.com>
2023-09-06 12:21:02 +0200

    Create _check_keydown_events() to move shiop

--- alien_invasion.py
+++ alien_invasion.py
@@ -38,10 +38,7 @@ class AlienInvasion:
 
             # Move the ship
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif  event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
 
             # Stop moving the ship
             elif event.type == pygame.KEYUP:
@@ -49,6 +46,15 @@ class AlienInvasion:
                     self.ship.moving_right = False
                 elif event.key == pygame.K_LEFT:
                     self.ship.moving_left = False
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    
 
     def _update_screen(self):
         """Update images on screen, flip to new screen."""
