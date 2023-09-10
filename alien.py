b7fb0b2fc7e2b5e70f900c80127d4b89b056263b
djkeu <mrkooij@gmail.com>
2023-09-10 11:19:09 +0200

    Initialize alien

--- alien.py
+++ alien.py
@@ -1,8 +1,24 @@
 import pygame
-from pygame.sprite import Sprite
+from pygame.sprite import _Group, Sprite
 
 
 class Alien(Sprite):
     """Class to represent a singel alien."""
 
-    
+    def __init__(self, ai_game) -> None:
+        """Initialize the alien and its starting position."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+
+        # Load alien image, set its rect attribute
+        self.image = pygame.image.load(self.settings.alien_image).convert_alpha()
+        self.rect = self.image.get_rect()
+
+        # Start new aliens near the top left of the screen
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Store the alien's exact horizontal position
+        self.x = float(self.rect.x)
+        
