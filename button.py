eb5d63132062221a5a65a611473b86d15089fd40
djkeu <mrkooij@gmail.com>
2023-10-15 18:21:57 +0200

    Create Button(), initialize attributes

--- button.py
+++ button.py
@@ -1,2 +1,23 @@
 import pygame.font
 
+class Button:
+    """Button to be used in Alien Invasion."""
+
+    def __init__(self, ai_game, msg) -> None:
+        """Initialize button attributes."""
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        # Set the dimensions and properties of the button
+        self.width, self.height = 200, 50
+        self.button_color = (0, 255, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # Build the button's rect object and center it
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = self.screen_rect.center
+
+        # The button message needs to be prepped only once
+        self._prep_msg(msg)
+        
