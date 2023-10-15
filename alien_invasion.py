b5ee30a44f3cfdbeb45f9c02e7457da9cd18e282
djkeu <mrkooij@gmail.com>
2023-10-15 18:34:07 +0200

    Draw button to screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -41,6 +42,9 @@ class AlienInvasion:
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
+
+        # Make the play-button
+        self.play_button = Button(self, "Play")
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -203,6 +207,10 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # Draw the button is the screen is inactive
+        if not self.stats.game_active:
+            self.play_button.draw_button()
 
         pygame.display.flip()
  
