493822b1f3abf70f7da87a86ae9b784c479c4bcf
djkeu <mrkooij@gmail.com>
2023-10-15 18:33:55 +0200

    Change button color

--- button.py
+++ button.py
@@ -10,7 +10,7 @@ class Button:
 
         # Set the dimensions and properties of the button
         self.width, self.height = 200, 50
-        self.button_color = (0, 255, 0)
+        self.button_color = (0, 100, 0)
         self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont(None, 48)
 
@@ -31,4 +31,3 @@ class Button:
         """Draw blank button, then draw mesage."""  
         self.screen.fill(self.button_color, self.rect)
         self.screen.blit(self.msg_image, self.msg_image_rect)
-        
